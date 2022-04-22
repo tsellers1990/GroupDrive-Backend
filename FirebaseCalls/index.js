7b67130249afe1e4b1b0c36066df38cbb8ebcd0d
tsellers1990 <tsellers1990@gmail.com>
2022-04-22 17:25:56 -0600

    addded package.json, routes, etc

---
+++ FirebaseCalls/index.js
@@ -0,0 +1,3 @@
+import * as DynamicRefs from './dynamicRefs';
+import * as StaticRefs from './staticRefs';
+
--- index.js
+++ index.js
@@ -4,9 +4,29 @@ const hostname = process.env.PORT || "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  // console.log({
+  //     req,
+  //     res
+  // })
+  console.log("path", req);
+
+  switch (req.url) {
+    case "/": {
+      res.statusCode = 200;
+    //   res.setHeader("Content-Type", "text/plain");
+      res.end("GroupDrive Backend Root");
+    }
+    case "/test": {
+      res.statusCode = 200;
+    //   res.setHeader("Content-Type", "text/plain");
+      res.end("GroupDrive Backend test");
+    }
+    default: {
+      res.statusCode = 200;
+    //   res.setHeader("Content-Type", "text/plain");
+      res.end("GroupDrive Backend default");
+    }
+  }
 });
 
 server.listen(port, hostname, () => {
