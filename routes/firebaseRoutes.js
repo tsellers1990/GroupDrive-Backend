fedb32ea83d2a766708087971403d24fcfbfd1d7
tsellers1990 <tsellers1990@gmail.com>
2022-05-06 20:53:13 -0600

    created some of the pub sub functionality

--- routes/firebaseRoutes.js
+++ routes/firebaseRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const admin = require("firebase-admin");
 
+let FIREBASE_CALL_DELAY = 2500;
+
 const serviceAccount =
   process.env.ENV_TYPE === "dev"
     ? require("../secret/serviceAccountKeyDev.json")
@@ -10,26 +12,55 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://groupdrive-94040.firebaseio.com",
 });
-// * these are all at /db/whatever
-
-// serviceAccountKeyDev.json
 
 const db = admin.database();
 
-router.get("/", (req, res) => {
-  db.ref("/").once("value", (snapshot) => {
-    console.log(snapshot.val());
+let bigObj = {};
+
+let loopCount = 0;
+
+const getDataBlock = async () => {
+  await db.ref("/").once("value", (snapshot) => {
+    const theData = snapshot.val();
+
+    if (
+      FIREBASE_CALL_DELAY !== theData.app_config.firebase_call_delay &&
+      theData.app_config.firebase_call_delay
+    ) {
+      FIREBASE_CALL_DELAY = theData.app_config.firebase_call_delay;
+    }
+
+    bigObj = theData;
+    loopCount++;
+    initLoop();
   });
+};
 
-  res.send("/db/*");
+const initLoop = () => {
+  setTimeout(() => {
+    console.log("looped this many times: " + loopCount);
+    getDataBlock();
+  }, FIREBASE_CALL_DELAY);
+};
+
+initLoop();
+
+// * Read Routes
+
+router.get("/", (req, res) => {
+  res.send(bigObj);
 });
 
-router.get("/1", (req, res) => {
-  res.send("/db/1");
+router.get("/users", (req, res) => {
+    res.send(bigObj.users);
 });
 
-// res.statusCode = 200;
-// res.setHeader("Content-Type", "text/plain");
-// res.end("GroupDrive Backend Root");
+router.get("/app_config", (req, res) => {
+    res.send(bigObj.app_config);
+});
+
+router.get("/live-data", (req, res) => {
+    res.send(bigObj['live-data']);
+});
 
 module.exports = router;
