dde95c8c5984ca179720906adc8f670fe821f994
Adam <me@adamksmith.xyz>
2022-06-01 20:03:22 -0600

    Broke out postgresClient so we arent initiating multiple database connections, added the friend operators class

---
+++ app/middleware/postgresClient.js
@@ -0,0 +1,16 @@
+const { Client } = require("pg");
+const e = require("express");
+const client = new Client({
+    user: 'postgres ',
+    host: 'postgres_database',
+    database: 'group_drive',
+    password: 'example',
+    port: 5432,
+})
+client.connect().then(()=>{
+    console.log("postgres db connection is successfull");
+}).catch((e)=>{
+    console.log("no connection to postgres, did something go wrong? ");
+});
+
+module.exports = client
--- app/middleware/userDatabaseOperations.js
+++ app/middleware/userDatabaseOperations.js
@@ -1,17 +1,4 @@
-const { Client } = require("pg");
-const e = require("express");
-const client = new Client({
-    user: 'postgres ',
-    host: 'postgres_database',
-    database: 'group_drive',
-    password: 'example',
-    port: 5432,
-})
-client.connect().then(()=>{
-    console.log("postgres db connection is successfull");
-}).catch((e)=>{
-    console.log("no connection to postgres, did something go wrong? ");
-});
+const {client} = require("../middleware/postgresClient")
 
 /*
 @name: createUser
