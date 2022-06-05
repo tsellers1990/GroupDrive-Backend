e3af55e084db70758f2b724b41c688b1aee7d0c1
asmith <asmith@everisthealth.com>
2022-06-05 01:29:06 -0600

    Flush out user table

--- app/migrations/1654399899550_users.js
+++ app/migrations/1654399899550_users.js
@@ -1,10 +1,51 @@
 /* eslint-disable camelcase */
 
 exports.shorthands = undefined;
-
+// create table users
+// (
+//     uid           varchar not null,
+//     "userName"    varchar not null,
+//     password      varchar not null,
+//     "carType"     varchar,
+//     "displayName" varchar not null,
+//     friends       character varying[],
+//     "numDrives"   integer default 0,
+//     "profileURL"  varchar
+// );
 exports.up = pgm => {pgm.createTable('users', {
-    id: 'id',
-    name: { type: 'varchar(1000)', notNull: true },
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: 'integer',
+    },
+    userName: { type: 'varchar(500)',
+        notNull: true,
+        unique: true,
+    },
+    password: {
+        type: 'varchar(10000)',
+        notNull: true
+    },
+    carType: {
+        type: 'varchar(1000)',
+        notNull: true,
+        defaultValue: 'MayBike',
+    },
+    displayName: {
+        type: 'varchar(50)',
+        notNull: true,
+    },
+    numDrives: {
+        type: "integer",
+        notNull: true,
+        defaultValue: 0,
+    },
+    profileURL: {
+        type: "varchar(255)",
+        notNull: true,
+        defaultValue: "https://res.cloudinary.com/selrich-technology/image/upload/v1653431979/ymztltnx1lgsynhqknw2.png"
+    },
     createdAt: {
         type: 'timestamp',
         notNull: true,
