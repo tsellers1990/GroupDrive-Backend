15289ce3875e5798b627d03cbb80040e5a83a4f8
asmith <asmith@everisthealth.com>
2022-06-05 02:03:54 -0600

    add local options to make things more durable

--- app/kickstart.sh
+++ app/kickstart.sh
@@ -6,6 +6,11 @@ echo "Migration is Complete!"
 
 #Lets setup our App for which environment we are in
 case $ENV in
+  local | LOCAL)
+    echo "Starting App in Local Mode! This will be Buggy!!!!!"
+    npm install
+    npm run start-dev
+    ;;
   #Are we running in a Dev Env?
   dev | DEV)
     echo "Starting App in Dev Mode"
