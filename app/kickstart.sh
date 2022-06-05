0ce634b8b100f649de2fd944dff3ac0f45fe1df6
asmith <asmith@everisthealth.com>
2022-06-05 02:10:21 -0600

    added more tweaks to the kickstart.sh

--- app/kickstart.sh
+++ app/kickstart.sh
@@ -1,29 +1,36 @@
 #!/usr/bin/env bash
 #Lets kick off a migration
-echo "Starting DB Migration!"
-until npm run migrate up >> /dev/null; do echo "Retrying Migration till DB is Ready..."; sleep 2; done
-echo "Migration is Complete!"
+migration(){
+  echo "Starting DB Migration!"
+  until npm run migrate up >> /dev/null; do echo "Retrying Migration till DB is Ready..."; sleep 2; done
+  echo "Migration is Complete!"
+}
+
 
 #Lets setup our App for which environment we are in
 case $ENV in
   local | LOCAL)
     echo "Starting App in Local Mode! This will be Buggy!!!!!"
     npm install
+    migration
     npm run start-dev
     ;;
   #Are we running in a Dev Env?
   dev | DEV)
     echo "Starting App in Dev Mode"
+    migration
     npm run start-dev
     ;;
   #Are we running in a QA Env?
   qa | QA)
     echo "Starting App in QA Mode"
+    migration
     npm run start-dev
     ;;
   #Are we running in a Production Env?
   prod | PROD)
     echo "Starting App in Production Mode"
+    migration
     npm run start-dev
    ;;
  #Error case statement, we should never hit this here!
