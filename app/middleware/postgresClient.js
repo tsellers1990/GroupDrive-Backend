ce9cfc0bc27f3150ba8b2dc4f7cc1b37c0fa7dca
Adam <me@adamksmith.xyz>
2022-06-07 16:57:44 -0600

    Clean up postgres env vars

--- app/middleware/postgresClient.js
+++ app/middleware/postgresClient.js
@@ -1,11 +1,6 @@
 const { Client } = require("pg");
 const e = require("express");
 const client = new Client({
-  user: "postgres",
-  host: "postgres_database",
-  database: "group_drive",
-  password: "example",
-  port: 5432,
 });
 client
   .connect()
