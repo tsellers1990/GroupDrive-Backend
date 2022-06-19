6e04f1b2d6063bebe5851b7f82cfd4db379de648
Adam <me@adamksmith.xyz>
2022-06-18 23:39:33 -0600

    testing secret checkouts

--- .space.kts
+++ .space.kts
@@ -4,7 +4,7 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
+job("Build and Deploy Backend") {
     docker {
         build {
             file = "./Dockerfile"
@@ -12,6 +12,15 @@ job("Hello World!") {
         }
         push("selrich.registry.jetbrains.space/p/groupdrive/container-artifacts/backend") {
             tags("0.0.\$JB_SPACE_EXECUTION_NUMBER")
+        }
+    }
+    container(displayName = "Show pwd", image = "ubuntu") {
+        env["k8s"] = Params("linode_k8s")
+
+        shellScript {
+            content = """
+                echo My password for ${'$'}k8s
+            """
         }
     }
 }
