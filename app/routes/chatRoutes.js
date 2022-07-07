18d099f94201a443bd45d23a6797607c86536139
tsellers1990 <tsellers1990@gmail.com>
2022-07-06 21:10:17 -0600

    demo

--- app/routes/chatRoutes.js
+++ app/routes/chatRoutes.js
@@ -3,6 +3,12 @@ const router = require("express").Router();
 // * Chats
 router.get("/chat", async (req, res) => {});
 
-router.put("/chat", async (req, res) => {});
+router.put("/chat", async (req, res) => {
+
+
+
+
+    
+});
 
 module.exports = router;
