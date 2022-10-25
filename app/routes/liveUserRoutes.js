e9eee4d4324111d0a46f7f5ac85ca5624da0b563
tsellers1990 <tsellers1990@gmail.com>
2022-10-24 21:06:48 -0600

    updated backend to match up with Front end

--- app/routes/liveUserRoutes.js
+++ app/routes/liveUserRoutes.js
@@ -18,15 +18,15 @@ router.get("/", async (req, res) => {
         res.sendStatus(500);
       });
   } else {
-    console.log("getting all userGeos");
+    // console.log("getting all userGeos");
     const locations = readLocation();
     locations
       .then((data) => {
         const arr = data
           .filter((userBlock) => {
-            // if (req.params.requesterUid !== userBlock._id) {
+            if (req.params.requesterUid !== userBlock._id) {
             return true;
-            // }
+            }
           })
           .map((dave) => {
             const { coordinate, _id, userName } = dave;
@@ -40,7 +40,7 @@ router.get("/", async (req, res) => {
         res.status(200).send(arr);
       })
       .catch((err) => {
-        console.log(err);
+        console.log("err", err);
         res.sendStatus(500);
       });
   }
@@ -48,22 +48,23 @@ router.get("/", async (req, res) => {
 
 // ! post route
 router.put("/", async (req, res) => {
-  console.log("req.body", req.query);
+  // console.log("req.body", req.query);
   const { uid, userName, coordinate, isOnline = true } = req.query;
-  console.log({ uid });
-  if(coordinate?.latitude && coordinate?.longitude){
-    const data = writeLocation(uid, userName, coordinate, isOnline);
+  // console.log(req.query);
+  const parseCoords = JSON.parse(coordinate)
+  if(parseCoords?.latitude && parseCoords?.longitude){
+    const response = await writeLocation(uid, userName, parseCoords, isOnline);
     //? also something to send off
-  
-    data
-      .then((data) => {
-        res.sendStatus(201);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.sendStatus(500);
-      });
+
+    console.log({response})
+    if (response) {
+      res.sendStatus(201)
+    } else {
+      res.sendStatus(500)
+    }
   } else {
+    // console.log("invalid coord.lat or long", coordinate["latitude"], coordinate.longitude)
+    // console.log(coordinate)
     res.sendStatus(500)
   }
 });
