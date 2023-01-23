893fc6917f0b05977739e0b71191f57b2e0de6e6
tsellers1990 <tsellers1990@gmail.com>
2023-01-22 21:35:10 -0700

    updated b/e w/ a few frontend api tweaks

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,26 +2,24 @@ const router = require("express").Router();
 
 const firebaseMiddle = require("../middleware/authMiddleware/index");
 
-
 const geoRoutes = require("./geoRoutes");
 const chatRoutes = require("./chatRoutes");
-const userRoutes = require("./userRoutes")
-const friendRoutes = require("./friendRoutes")
-const driveRoutes = require("./driveRoutes")
-const liveUserRoutes = require("./liveUserRoutes")
+const userRoutes = require("./userRoutes");
+const friendRoutes = require("./friendRoutes");
+const driveRoutes = require("./driveRoutes");
+const eventRoutes = require("./eventRoutes");
+const liveUserRoutes = require("./liveUserRoutes");
 const authRoutes = require("./authRoutes");
 
-
-router.use("/geo", geoRoutes)
-router.use("/chat", chatRoutes)
-router.use("/user", userRoutes)
-router.use("/friend", friendRoutes)
-router.use("/drive", driveRoutes)
-router.use("/liveLocation", liveUserRoutes)
+router.use("/geo", geoRoutes);
+router.use("/chat", chatRoutes);
+router.use("/user", userRoutes);
+router.use("/friend", friendRoutes);
+router.use("/drive", driveRoutes);
+router.use("/event", eventRoutes);
+router.use("/liveLocation", liveUserRoutes);
 
 // router.use(firebaseMiddle.decodeToken)
-router.use("/auth", authRoutes)
+router.use("/auth", authRoutes);
 
-
-
-module.exports = router
+module.exports = router;
